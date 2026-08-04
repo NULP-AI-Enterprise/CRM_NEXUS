a2bc9ef8af2b62e34de1e12c30278dd1d22d751d
yorza-theasis <yorza@thesis-i.com>
2026-08-04 22:06:14 +0300

    Delete migration.sql

--- prisma/migrations/20260804214000_add_user_id_to_contact_connection/migration.sql
+++
@@ -1,9 +0,0 @@
--- AlterTable
-ALTER TABLE "ContactConnection" ADD COLUMN IF NOT EXISTS "userId" TEXT NOT NULL;
-
--- CreateIndex
-CREATE INDEX IF NOT EXISTS "ContactConnection_userId_idx" ON "ContactConnection"("userId");
-
--- AddForeignKey
-ALTER TABLE "ContactConnection" ADD CONSTRAINT "ContactConnection_userId_fkey" 
-FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
